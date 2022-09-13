002f9b4d20223d503cf9e50d50a8cc01baefe4a5
Jasun Thomas <jasun.t@gmail.com>
2022-09-13 10:43:44 -0600

    finished sync with updated dates

--- sync_cals.py
+++ sync_cals.py
@@ -52,7 +52,11 @@ if __name__ == "__main__":
     
     User = get_user_model()
     
-    db_events = AllEvents.objects.all()
+    db_events = AllEvents.objects.all()  
+    
+    # simple dict to see if cals have a new updated date
+    db_event_updated_dates = {x.cal_id: x.updated for x in db_events}
+    
     profiles = CalendarProfile.objects.all()
 
     combine_calId_dict = {}  # dictionary to hold owner ID and each their 
@@ -60,7 +64,12 @@ if __name__ == "__main__":
     all_inserted_events = []
     
     ev_count = 0
+    inserted_count = 0
+    updated_count = 0
     
+    events_to_update = []  # just the ID for which will get updated to EVERY user if it lands here.
+    events_to_insert = []  # HERE to insert them, by cal-id
+
     for user in profiles:
         # each shared calendar ID
         sharedids = []
@@ -78,23 +87,43 @@ if __name__ == "__main__":
             if id:
                 calevents = grab_events(service, id)
             if calevents:
-                ev_count += len(calevents)
+                # ev_count += len(calevents)
                 # format them ready for db
                 for event in calevents:
+                    ev_count += 1
                     print(f"is there 'cal_id' field? {event.keys()}")
-                    shaped = shape_event(event)
-                    shaped['owner'] = usr_model[0]
-                    db_rcpt, entered = event_to_db(shaped)  # use this rcpt to enter back cal_ids for other users insert
-                    if entered:
+                    if event['id'] in db_event_updated_dates.keys():
+                        print(f"event IS in db. Does updated date match?")
+                        if event['updated'] == db_event_updated_dates[event['id']]:
+                            print(f"date is the same. skip it. {event['summary']}")
+                            continue
+                        else:
+                            print(f"event {event['summary']} is new. Add it.")
+                            shaped = shape_event(event)
+                            shaped['owner'] = usr_model[0]
+                            db_rcpt, entered = event_to_db(shaped) 
+                            all_inserted_events.append(db_rcpt)
+                            events_to_update.append(event['cal_id'])
+                            updated_count += 1
+                    else:
+                        print(f"event {event['summary']} is new. Add it.")
+                        shaped = shape_event(event)
+                        shaped['owner'] = usr_model[0]
+                        db_rcpt, entered = event_to_db(shaped)  # use this rcpt to enter back cal_ids for other users insert
                         all_inserted_events.append(db_rcpt)
-                    print(f"to db? {entered}")
+                        inserted_count += 1
+                        events_to_insert.append(event['cal_id'])
+                        print(f"to db? {entered}")
                 # insert to db
                 print(f" sum of all events so far: {ev_count}")
-        
+    
     # refresh db_Events and start working them into user shared cals
     db_events = AllEvents.objects.all()
     # loop db events, need to check either:
     # A: event_ID from user is on the event yet, and send 'patch' if it does.
+
+    all_to_update = events_to_insert + events_to_update
+
     for item in db_events:
         # remove whos the owner from the combine_calID_dict so it doesn't send to them.
         # owner = combine_calId_dict.pop(item.owner.id) ## BAD IDEA. need this for later
@@ -111,12 +140,18 @@ if __name__ == "__main__":
 
         if event_ids:
             for k, evid in event_ids.items():
+                if item.cal_id not in all_to_update:
+                    print('does not need update.')
+                    continue
                 rcp = patch_event(service, non_owners[k], evid, item.__dict__)
                 print(f'updates went successfully. {rcp["id"]}')
         else:
             # B: if not, do INSERT event if NOT the owner.
             event_ids = {} 
             for k, calid in non_owners.items():
+                if item.cal_id not in all_to_update:
+                    print('does not need insert.')
+                    continue
                 rcpt = insert_event(service, calid, item.__dict__)
                 event_ids[k] = rcpt['id']
             item.other_ids = event_ids
